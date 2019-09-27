ae6b77b0f4d6d5b8d949a66368bc51e594d3db94
Tetiana Melnyk <prostotayna@gmail.com>
2019-09-27 15:32:28 +0300

    db config from env vars

--- config/config.js
+++ config/config.js
@@ -15,10 +15,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+    host: process.env.HOSTNAME || 'remotemysql.com',
+    port: process.env.MYSQL_PORT || 3306,
     dialect: 'mysql',
   },
 };
