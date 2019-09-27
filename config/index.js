616371b0cf7e01ce1b08479e316f3acdc633a19c
Tetiana Melnyk <prostotayna@gmail.com>
2019-09-27 16:24:43 +0300

    Change callbacks for socials auth

--- config/index.js
+++ config/index.js
@@ -5,21 +5,24 @@ module.exports = {
   facebook: {
     clientId: process.env.FACEBOOK_CLIENT_ID,
     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callback: 'http://localhost:5001/api/v1/auth/facebook/callback',
+    callback:
+      'https://peaceful-fortress-18867.herokuapp.com/api/v1/auth/facebook/callback',
     scopes: ['email', 'public_profile'],
   },
 
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callback: 'http://localhost:5001/api/v1/auth/google/callback',
+    callback:
+      'https://peaceful-fortress-18867.herokuapp.com/api/v1/auth/google/callback',
     scopes: ['email', 'profile'],
   },
 
   linkedIn: {
     clientId: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    callback: 'http://localhost:5001/api/v1/auth/linkedin/callback',
+    callback:
+      'https://peaceful-fortress-18867.herokuapp.com/api/v1/auth/linkedin/callback',
     scopes: ['r_emailaddress', 'r_liteprofile'],
   },
 
