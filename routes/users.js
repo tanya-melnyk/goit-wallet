bffa930b9370c59158f02ba6c1d3ed7f77be7d12
Tetiana Melnyk <prostotayna@gmail.com>
2019-09-27 16:45:56 +0300

    Fix register error handler

--- routes/users.js
+++ routes/users.js
@@ -10,12 +10,12 @@ const { createUser, deleteUserById, getUsers } = require('../controllers/user');
 router.post('/', async (req, res) => {
   try {
     const user = await createUser(req.body);
-    
+
     res.status(201).render('dashboard', { user });
     // res.status(201).json({ status: 'OK', user: user.render() });
   } catch (err) {
-    // return res.status(500).send({ Error: err.name, message: err.message });
-    return res.status(500).redirect('/api/v1/login');
+    return res.status(500).send({ Error: err.name, message: err.message });
+    // return res.status(500).redirect('/api/v1/login');
   }
 });
 
